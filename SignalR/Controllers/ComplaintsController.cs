1617bf9c95f1ea0e748affa8dfb1d1d856e26f2a
Kenely Oliveira <kenely.oliveira@gmail.com>
2018-09-16 10:07:43 -0300

    Initial commit

---
+++ SignalR/Controllers/ComplaintsController.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SignalR;
+
+namespace SignalR
+{
+    public class ComplaintsController : ApiControllerWithHub<MyHub>
+  {
+        private MordorEntities db = new MordorEntities();
+
+        // GET: api/Complaints
+        public IQueryable<CUSTOMER_COMPLAINTS> GetCUSTOMER_COMPLAINTS()
+        {
+            return db.CUSTOMER_COMPLAINTS;
+        }
+
+        // GET: api/Complaints/5
+        [ResponseType(typeof(CUSTOMER_COMPLAINTS))]
+        public IHttpActionResult GetCUSTOMER_COMPLAINTS(string id)
+        {
+            CUSTOMER_COMPLAINTS cUSTOMER_COMPLAINTS = db.CUSTOMER_COMPLAINTS.FirstOrDefault<CUSTOMER_COMPLAINTS>(x => x.CUSTOMER_ID == id);
+            if (cUSTOMER_COMPLAINTS == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cUSTOMER_COMPLAINTS);
+        }
+
+        // PUT: api/Complaints/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCUSTOMER_COMPLAINTS(int id, CUSTOMER_COMPLAINTS cUSTOMER_COMPLAINTS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cUSTOMER_COMPLAINTS.COMPLAINT_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cUSTOMER_COMPLAINTS).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CUSTOMER_COMPLAINTSExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Complaints
+        [ResponseType(typeof(CUSTOMER_COMPLAINTS))]
+        public IHttpActionResult PostCUSTOMER_COMPLAINTS(CUSTOMER_COMPLAINTS cUSTOMER_COMPLAINTS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CUSTOMER_COMPLAINTS.Add(cUSTOMER_COMPLAINTS);
+            db.SaveChanges();
+
+            var subscribed = Hub.Clients.Group(cUSTOMER_COMPLAINTS.CUSTOMER_ID);
+            subscribed.addItem(cUSTOMER_COMPLAINTS);
+
+      return CreatedAtRoute("DefaultApi", new { id = cUSTOMER_COMPLAINTS.COMPLAINT_ID }, cUSTOMER_COMPLAINTS);
+        }
+
+        // DELETE: api/Complaints/5
+        [ResponseType(typeof(CUSTOMER_COMPLAINTS))]
+        public IHttpActionResult DeleteCUSTOMER_COMPLAINTS(int id)
+        {
+            CUSTOMER_COMPLAINTS cUSTOMER_COMPLAINTS = db.CUSTOMER_COMPLAINTS.Find(id);
+            if (cUSTOMER_COMPLAINTS == null)
+            {
+                return NotFound();
+            }
+
+            db.CUSTOMER_COMPLAINTS.Remove(cUSTOMER_COMPLAINTS);
+            db.SaveChanges();
+
+            return Ok(cUSTOMER_COMPLAINTS);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CUSTOMER_COMPLAINTSExists(int id)
+        {
+            return db.CUSTOMER_COMPLAINTS.Count(e => e.COMPLAINT_ID == id) > 0;
+        }
+    }
+}
---
+++ SignalR/Hubs/MyHub.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR
+{
+  public class MyHub : Hub
+  {
+    public void Subscribe(string customerId)
+    {
+      Groups.Add(Context.ConnectionId, customerId);
+    }
+
+    public void Unsubscribe(string customerId)
+    {
+      Groups.Remove(Context.ConnectionId, customerId);
+    }
+  }
+}
